bd0275b45e2c48e4e6d91a5cfce212f563b0f20c
Jonathan Hernandez <hernandez.jonathan95@gmail.com>
2022-06-05 16:42:58 -0400

    DB path files

---
+++ Scheduler/Scheduler/Common/Global.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduler.Common
+{
+    public static class Global
+    {
+        public static string systemPath = System.Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+        public static string masterDBPath = Path.Combine(systemPath, "files\\DB.txt");
+    }
+}
